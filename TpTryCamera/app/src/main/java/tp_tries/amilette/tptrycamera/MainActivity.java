8ccc724879d89a9382a78b172d10d81f63ad1f8c
David Pelletier <jedi_david_20@hotmail.com>
2015-09-16 12:19:39 -0400

    Ajout de la prise en charge du changement linguistique (Non terminé, car il faut refaire l'activivé options et Aide)

--- TpTryCamera/app/src/main/java/tp_tries/amilette/tptrycamera/MainActivity.java
+++ TpTryCamera/app/src/main/java/tp_tries/amilette/tptrycamera/MainActivity.java
@@ -1,14 +1,14 @@
 package tp_tries.amilette.tptrycamera;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import tp_tries.amilette.tptrycamera.services.MyService;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 
     Button boutonJouer;
