971cc5c4a57d59e3d0b0a3df1e93c7e723d2f53d
Anne-Sophie Milette <asmilette@gmail.com>
2015-09-20 21:09:37 -0400

    Video entrer + tout mettre en portrait

---
+++ TpTryCamera/app/src/main/java/tp_tries/amilette/tptrycamera/VideoEnter.java
@@ -0,0 +1,117 @@
+package tp_tries.amilette.tptrycamera;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import tp_tries.amilette.tptrycamera.services.MyService;
+
+public class VideoEnter extends Activity {
+
+    private VideoView myVideoView;
+    private int position = 0;
+    private ProgressDialog progressDialog;
+    private MediaController mediaControls;
+    Intent it;
+    MediaPlayer.OnCompletionListener mCompletionListener;
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // set the main layout of the activity
+        setContentView(R.layout.activity_video_enter);
+
+        //set the media controller buttons
+        if (mediaControls == null) {
+            mediaControls = new MediaController(VideoEnter.this);
+        }
+
+        //initialize the VideoView
+        myVideoView = (VideoView) findViewById(R.id.video_view);
+
+        // create a progress bar while the video file is loading
+        progressDialog = new ProgressDialog(VideoEnter.this);
+        // set a title for the progress bar
+        progressDialog.setTitle("Minion Attack");
+        // set a message for the progress bar
+        progressDialog.setMessage("Loading...");
+        //set the progress bar not cancelable on users' touch
+        progressDialog.setCancelable(false);
+        // show the progress bar
+        progressDialog.show();
+
+        try {
+            //set the media controller in the VideoView
+            myVideoView.setMediaController(mediaControls);
+
+            //set the uri of the video to be played
+            myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.minionvideo));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        myVideoView.requestFocus();
+
+
+        //we also set an setOnPreparedListener in order to know when the video file is ready for playback
+        myVideoView.setOnPreparedListener(new OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                // close the progress bar and play the video
+                progressDialog.dismiss();
+                //if we have a position on savedInstanceState, the video playback should start from here
+                myVideoView.seekTo(position);
+                if (position == 0) {
+                    myVideoView.start();
+                } else {
+                    //if we come from a resumed activity, video playback will be paused
+                    myVideoView.pause();
+                }
+            }
+        });
+          mCompletionListener =  new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+
+                        if (mCompletionListener != null) {
+                            it =    new Intent(VideoEnter.this, MainActivity.class);
+                            startService(it);
+                        }
+                    }
+                };
+
+        myVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+
+                it =    new Intent(VideoEnter.this, MainActivity.class);
+                startActivity(it);
+            }
+        });
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        //we use onSaveInstanceState in order to store the video playback position for orientation change
+        savedInstanceState.putInt("Position", myVideoView.getCurrentPosition());
+        myVideoView.pause();
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        //we use onRestoreInstanceState in order to play the video playback from the stored position
+        position = savedInstanceState.getInt("Position");
+        myVideoView.seekTo(position);
+    }
+}
